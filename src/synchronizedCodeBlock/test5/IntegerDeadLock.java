9d4fae36b98aa6c2d8b6b84a4f41a084db9bd2d6
zhujiating <zhujiating@omatec.cn>
2017-08-29 19:44:55 +0800

    线程间通信

---
+++ src/synchronizedCodeBlock/test5/IntegerDeadLock.java
@@ -0,0 +1,7 @@
+package synchronizedCodeBlock.test5;
+
+/**
+ * Created by zhujiating on 2017/8/28.
+ */
+public class IntegerDeadLock {
+}
---
+++ src/waitAndNitify/WaitTest.java
@@ -0,0 +1,7 @@
+package waitAndNitify;
+
+/**
+ * Created by zhujiating on 2017/8/29.
+ */
+public class WaitTest {
+}
